d425c4c010c186750d2e5d09df2d88e9e22a9dce
sanamehrban <75242720+sanamehrban@users.noreply.github.com>
2021-11-01 20:12:58 +0500

    Add project files.

---
+++ oldhouse/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using oldhouse.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace oldhome.Controllers
+{
+    public class HomeController : Controller
+    {
+        OldHouseEntities DB = new OldHouseEntities();
+        // GET: Home
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(MyLogin login)
+        {
+            Register r = DB.Registers.Where(a => a.UserName == login.UserName && a.Password == login.Password).FirstOrDefault();
+            if (r != null)
+            {
+                return View("OldHome");
+            }
+            return View();
+
+        }
+        public ActionResult OldHome()
+        {
+              return View("AddDetails");
+
+        }
+
+        public ActionResult AddDetails()
+        {
+            return View("");
+
+        }
+
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(MyRegister register)
+        {
+            Register r = new Register()
+            {
+                FullName = register.FullName,
+                ContactNo = register.ContactNo,
+                Email = register.Email,
+                City = register.City,
+                Address = register.Address,
+                UserName = register.UserName,
+                Password = register.Password,
+                UserType = register.UserType
+            };
+            DB.Registers.Add(r);
+            DB.SaveChanges();
+
+            return View();
+            
+
+
+        }
+    }
+}
+     
+       
+       
+      
+   
---
+++ oldhouse/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="oldhouse.MvcApplication" Language="C#" %>
